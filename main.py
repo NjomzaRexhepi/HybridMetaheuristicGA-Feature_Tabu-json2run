862199a34abff412d6646d3ee8490dc14de180f5
NjomzaRexhepi <njomzaarexhepii@gmail.com>
2025-06-27 23:57:20 +0200

    neps

--- main.py
+++ main.py
@@ -178,10 +178,10 @@ if __name__ == "__main__":
     print("Cuda:", torch.cuda.is_available())
     print("Running Neural Pipeline Search for Genetic Algorithm Hyperparameter Optimization")
     parser = argparse.ArgumentParser(description="Run Genetic Algorithm with NePS hyperparameter optimization.")
-    parser.add_argument("--input_file", type=str, default="B6k_L35_D70.txt", help="Input File")
+    parser.add_argument("--input_file", type=str, default="B9k_L40_D80.txt", help="Input File")
     parser.add_argument("--max_evaluations", type=int, default=10,
                         help="Maximum number of hyperparameter configurations to try.")
-    parser.add_argument("--neps_root_dir", type=str, default="neps_results_B6k_L35_D70_output",
+    parser.add_argument("--neps_root_dir", type=str, default="neps_results_B9k_L40_D80_output",
                         help="Directory to store NePS results.")
  
     args = parser.parse_args()
