6c1b46b31178c589b0c239c7680dcf349169b086
NjomzaRexhepi <njomzaarexhepii@gmail.com>
2025-06-29 07:06:55 +0200

    neps

--- models/genetic_solver.py
+++ models/genetic_solver.py
@@ -90,7 +90,7 @@ class GeneticAlgorithmSolver:
             best_score = best_solution.fitness_score
 
             no_improvement_counter = 0
-            patience = 10
+            patience = 6
 
             for generation in range(self.population_size):
                 print(f"Gen {generation}: Best fitness = {best_score}")
